1be01cd329fd74d80dfcfb533a623b12f105e672
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-23 08:02:44 +0100

    changes in script, does not work

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-"use strict"
+"use strict";
 
-var fs = require('fs'); //modul fs- odpowiada za zapis(fs.writeFile i odczyt fs.readFile)
-var http = require('http');
+var fs = require("fs"); //modul fs- odpowiada za zapis(fs.writeFile i odczyt fs.readFile)
+var http = require("http");
 var server = http.createServer();
 
-server.on('request', function (request, response) {
-    response.setHeader('Content-Type', 'photo/jpg; charset=utf-8');
-    if (request.method === 'GET' && request.url === '/') {
-        fs.readFile('./index.html', 'utf-8', function (err, data) {
-            response.write(data);
-        });
+server
+  .on("request", function(request, response) {
+    if (request.method === "GET" && request.url === "/") {
+      fs.readFile("./index.html", function(err, html) {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        response.write(html);
         response.end();
+      });
     } else {
-        fs.readFile('./photo.jpg', function (err, data) {
-            response.write(data);
-        })
-        //fs.readFile('./photo.jpg', function (err, data) {
-        //	response.write('<img src="./photo.jpg">');
-        //})
-
-        //readFileSync ????? co to
+      fs.readFile("./error.jpg", function(err, img) {
+        response.writeHead(404, { "Content-Type": "image/jpg" });
+        response.write(img);
         response.end();
+      });
     }
-})
-server.listen(5500);
-//http://localhost:5500
+  })
+  .listen(9000);
