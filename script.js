570e61ea7a4856a522a712f503bc4a38b8978e29
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-02-25 08:37:32 +0100

    some changes, does not work

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 "use strict";
 
 var fs = require("fs"); //modul fs- odpowiada za zapis(fs.writeFile i odczyt fs.readFile)
+var StatMode = require('stat-mode');
 var http = require("http");
 var server = http.createServer();
 
 server
-  .on("request", function(request, response) {
+  .on("request", function (request, response) {
     if (request.method === "GET" && request.url === "/") {
-      fs.readFile("./index.html", function(err, html) {
+      fs.readFile("./index.html", function (err, html) {
         response.setHeader("Content-Type", "text/html; charset=utf-8");
         response.write(html);
         response.end();
       });
     } else {
-      fs.readFile("./error.jpg", function(err, img) {
-        response.writeHead("./error.jpg", { "Content-Type": "image/jpg" });
+      fs.readFile("./error.jpg", function (err, img) {
+        response.writeHead(404, {
+          "Content-Type": "image/jpg"
+        });
         response.write(img);
         response.end();
       });
